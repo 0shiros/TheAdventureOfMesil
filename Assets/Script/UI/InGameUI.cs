7487324717b58f54d7f9b71eed9ceffe1729869b
0shiros <133508599+0shiros@users.noreply.github.com>
2025-03-20 10:13:48 +0100

    Add PauseMenu In Game

---
+++ Assets/Script/UI/InGameUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            PauseMenu();
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    public void SetGamePause(bool isPaused)
+    {
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void PauseMenu()
+    {
+        if(_pauseMenu.activeSelf)
+        {
+            _pauseMenu.SetActive(false);
+            SetGamePause(false);
+        }
+        else
+        {
+            _pauseMenu.SetActive(true);
+            SetGamePause(true);
+        }
+    }
+}
